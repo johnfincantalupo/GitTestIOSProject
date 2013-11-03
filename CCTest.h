7d5aadabf1e7880fa646fa476d3144fb50bf90f7
johnfincantalupo <john.incantalupo@gmail.com>
2013-11-03 11:56:49 -0500

    add a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by John Incantalupo on 11/3/13.
+//  Copyright (c) 2013 Incantalupo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by John Incantalupo on 11/3/13.
+//  Copyright (c) 2013 Incantalupo. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
